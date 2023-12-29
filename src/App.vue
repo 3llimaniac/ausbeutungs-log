49f460031dd06a0b7aa288d5ca6742e1dfa4a310
Elias <eliasheigl753@gmail.com>
2023-12-29 22:49:37 +0100

    started structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+import CalendarBar from './components/CalendarBar.vue'
+import CalendarBox from './components/CalendarBox.vue'
 </script>
 
 <template>
-  <div>Test</div>
+  <div class="h-screen p-32 pt-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 overflow-hidden">
+    <NavBar />
+    <CalendarBar />
+    <CalendarBox />
+  </div>
 </template>
