d50c671b6e113f9ecf09fccd85004354db25f732
Elias <eliasheigl753@gmail.com>
2023-12-31 00:22:44 +0100

    started HTML

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import UserBox from './components/UserBox.vue'
 import NavBar from './components/NavBar.vue'
 import CalendarBar from './components/CalendarBar.vue'
 import CalendarBox from './components/CalendarBox.vue'
 </script>
 
 <template>
-  <div class="h-screen p-32 pt-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 overflow-hidden">
+  <div class="h-screen p-32 pt-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 overflow-hidden">
+    <UserBox />
     <NavBar />
     <CalendarBar />
     <CalendarBox />
